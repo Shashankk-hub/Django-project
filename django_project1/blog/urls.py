f28b28ee874e0b9fac6b73291a6ca950f226251e
shashank <shashank877kumar@gmail.com>
2026-03-24 12:44:48 +0530

    Fix IntegrityError on new post creation

--- django_project1/blog/urls.py
+++ django_project1/blog/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 from . import views
-from.views import PostListView,PostDetailView
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_new'),
+    path('about/', views.about, name='about-page'),
+      # Home / List of posts
+    path('', views.PostListView.as_view(), name='blog-home'),
+    
+    # Detail view for a single post
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    
+    # Create a new post (requires logged-in user)
+    path('post/new/', views.PostCreateView.as_view(), name='post_new'),
+    
+    # About page
     path('about/', views.about, name='about-page'),
 ]
--- django_project1/blog/views.py
+++ django_project1/blog/views.py
@@ -1,44 +1,36 @@
 from django.shortcuts import render
-from blog.models import Post
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .models import Post
 
+# Home / List view
 class PostListView(ListView):
-       model = Post
-       template_name = 'blog/home.html'
-       context_object_name = 'posts'
-       ordering = ['-date_posted']
-       
-       
+    model = Post
+    template_name = 'blog/home.html'  # Template for the list
+    context_object_name = 'posts'
+    ordering = ['-date_posted']       # newest first
 
-def home(request):
-   context = {
-   'posts': Post.objects.all()
-   }
-   return render(request, 'blog/home.html', context)
-
-# def home(request):
-#    context = {
-#    'posts': posts
-#    }
-   
-def about(request):
-   return render(request, 'blog/about.html', {'title': 'About'})
-
-
-# posts = [
-# {
-#    'author': 'shashank',
-#    'title': 'Blog Post 1',
-#    'content': 'Blog 1 Content!',
-#    'date_posted': 'January 20, 2026'
-# },
-# {
-#    'author': 'aryan',
-#    'title': 'Blog Post 2',
-#    'content': 'Blog 2 Content!',
-#    'date_posted': 'January 23, 2026'
-# }
-# ]
-
+# Detail view for a single post
 class PostDetailView(DetailView):
     model = Post
+    template_name = 'blog/post_detail.html'
+
+# Create view for a new post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'  # Template for the form
+    fields = ['title', 'content']          # Fields displayed in the form
+    success_url = reverse_lazy('blog-home')  # Redirect after submit
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# Optional function-based views
+def home(request):
+    posts = Post.objects.all().order_by('-date_posted')
+    return render(request, 'blog/home.html', {'posts': posts})
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
